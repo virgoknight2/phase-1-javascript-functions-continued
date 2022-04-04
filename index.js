918c0ded9f627c60ca57772f67aca885517e8ce5
Lorelai Knutti <virgoknight2@gmail.com>
2022-04-04 19:04:41 -0400

    commit tree 26fff9ab9bfdf86e1b3e983526d181db5a661733

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+
+function saturdayFun(fun='roller-skate'){
+    return (`This Saturday, I want to ${fun}!`);
+};
+saturdayFun();
+  function mondayWork(work="go to the office"){
+      return(`This Monday, I will ${work}.`);
+  };
+  mondayWork();
+  function wrapAdjective(symbol) { 
+        const start= "You are";
+        return function(wrappedText){
+            return (`${start} ${symbol}${wrappedText}${symbol}!`);
+        };
+    };
+wrapAdjective("%")("a dedicated programer");
